109abda9049f7a54e04988077f7a59bbd86d2a4e
infuntis <infuntis@MacBook-Pro.local>
2017-01-15 18:23:39 +0300

    first commit

---
+++ src/GameField.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by infuntis on 15/01/17.
+ */
+public class GameField extends JPanel {
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = 400;
+    private Image dot;
+    private Image apple;
+    private int appleX;
+    private int appleY;
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+    private int dots;
+    private Timer timer;
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    private boolean inGame = true;
+
+
+    public GameField(){
+        setBackground(Color.black);
+        loadImages();
+    }
+
+    public void loadImages(){
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
+    }
+}
